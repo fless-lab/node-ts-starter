2d4e2ad6286b15f1a3fc9ddc39f3e23b8cf3fad6
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-06-29 23:57:55 +0000

    chore: commitlint configs update to authorize footer in commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -66,6 +66,6 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
-    'footer-empty': [2, 'always'],
+    // 'footer-empty': [2, 'always'],
   },
 };
